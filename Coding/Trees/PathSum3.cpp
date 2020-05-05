6fe7819a5e8c2249bc9c591943e3341300eb56fb
Sheakr <cs15btech11015@iith.ac.in>
2020-05-05 09:20:35 +0530

    complement

--- Coding/Trees/PathSum3.cpp
+++ Coding/Trees/PathSum3.cpp
@@ -9,15 +9,12 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-    
+class Solution {  
 private:
     int path(TreeNode* root,int sum){
         if(root == NULL) return 0;
         //if(sum == root->val) return 1;
-        //if(sum < 0 ) return 0;
-        
-        
+        //if(sum < 0 ) return 0; 
         return (root->val == sum ? 1 : 0)+path(root->left,sum-root->val)+path(root->right,sum-root->val);
     }
 public:
