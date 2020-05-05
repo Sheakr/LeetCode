fa964944d04b2e5f3e788ccf993495f9007c27f8
Srinivas D <37019002+Sheakr@users.noreply.github.com>
2020-05-05 13:36:43 +0530

    F3 (#3)
    * ransom magazine
    
    * Revert "ransom magazine"
    
    This reverts commit 2de571bc0997df29fd5ca86ed5c30da42e5d9447.
    
    * adding ransom
    
    * changing folder structure
    
    * changing codeFactor Commits
    
    * complement
    
    * firstUniqueCharacter

---
+++ Coding/May_challenge_2020/ransomNote_may_3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int *A = new int[26];
+        for(int i =0;i<26;i++){
+            A[i] = 0;
+        }
+        for(int i =0;i<magazine.length();i++){
+            A[magazine[i]-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            if(A[ransomNote[i]-'a'] > 0){
+                A[ransomNote[i]-'a']--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;       
+    }
+};
